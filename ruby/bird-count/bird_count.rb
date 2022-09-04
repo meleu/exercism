b1dd82180fe52d41c00066462ad888aed32c3f61
meleu <meleu.dev@gmail.com>
2022-09-04 13:39:22 -0300

    update

---
+++ ruby/bird-count/bird_count.rb
@@ -0,0 +1,26 @@
+# https://exercism.org/tracks/ruby/exercises/bird-count
+class BirdCount
+  def self.last_week
+    [0, 2, 5, 3, 7, 8, 4]
+  end
+
+  def initialize(birds_per_day)
+    @birds_per_day = birds_per_day
+  end
+
+  def yesterday
+    @birds_per_day[-2]
+  end
+
+  def total
+    @birds_per_day.sum
+  end
+
+  def busy_days
+    @birds_per_day.count { |number| number >= 5 }
+  end
+
+  def day_without_birds?
+    @birds_per_day.any?(&:zero?)
+  end
+end
