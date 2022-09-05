4a06528bf18e6187a0945b94e9d363712bde543d
meleu <meleu.dev@gmail.com>
2022-09-05 19:41:33 -0300

    update

---
+++ ruby/list-ops/list_ops.rb
@@ -0,0 +1,18 @@
+# https://exercism.org/tracks/ruby/exercises/list-ops
+class ListOps
+  def self.arrays(array)
+    array.length
+  end
+
+  def self.reverser(array)
+    array.reverse
+  end
+
+  def self.concatter(array1, array2)
+    array1 + array2
+  end
+
+  def self.mapper(array)
+    array.map
+  end
+end
