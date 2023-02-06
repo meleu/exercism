32ea16bd6a5ce02cdc8ea9454f8580e670ad2610
meleu <meleu.dev@gmail.com>
2023-02-06 10:22:09 -0300

    update

--- javascript/README.md
+++ javascript/README.md
@@ -6,6 +6,8 @@ In this file I'm taking notes about the interesting things I'm learning on each 
 
 ---
 
+## Bird Watcher
+
 - [bird-watcher](bird-watcher)
 
 Difference between `in` and `of`:
@@ -23,4 +25,56 @@ for (const key in myArray) console.log(key);
 
 for (const key of myArray) console.log(key);
 // output: a b c
+```
+
+---
+
+## Mixed Juices
+
+- [mixed-juices](mixed-juices)
+
+In this exercise I learned that in a switch statement the comparison is done
+by checking for strict equality `===`. (ok, it looks kinda obvious after it's
+said, but it was useful to be fully conscious about that.)
+
+
+---
+
+## Amusement Park
+
+- [amusement-park](amusement-park)
+
+### Optional Chaining
+
+Using the optional chaining operator `?.` we can prevent crashes when trying to access properties of an undefined object.
+
+```js
+const obj = {
+  address: {
+    street: 'My Own Street',
+    city: 'New York',
+  },
+};
+
+obj.address.zipCode;
+// => undefined
+
+// CRASH! Because 'residence' is not a property of 'obj'.
+obj.residence.street;
+// => TypeError: Cannot read property 'street' of undefined
+
+obj.residence?.street;
+// => undefined
+```
+
+
+### Nullish Coalescing
+
+Useful when we want to provide a default value in case a variable is `null` or `undefined`.
+
+```js
+let name; // <- undefined
+let person = name ?? 'World';
+console.log('Hello ' + person);
+// => Hello World
 ```
--- javascript/amusement-park/amusement-park.js
+++ javascript/amusement-park/amusement-park.js
@@ -10,7 +10,7 @@
  * @returns {Visitor} the visitor that was created
  */
 export function createVisitor(name, age, ticketId) {
-  throw new Error('Please implement the createVisitor function.');
+  return { name, age, ticketId };
 }
 
 /**
@@ -20,7 +20,8 @@ export function createVisitor(name, age, ticketId) {
  * @returns {Visitor} the visitor without a ticket
  */
 export function revokeTicket(visitor) {
-  throw new Error('Please implement the revokeTicket function.');
+  visitor.ticketId = null;
+  return visitor;
 }
 
 /**
@@ -31,7 +32,16 @@ export function revokeTicket(visitor) {
  * @returns {string} ticket status
  */
 export function ticketStatus(tickets, ticketId) {
-  throw new Error('Please implement the ticketStatus function.');
+  let name = tickets[ticketId];
+
+  switch (name) {
+    case undefined:
+      return 'unknown ticket id';
+    case null:
+      return 'not sold';
+    default:
+      return `sold to ${name}`;
+  }
 }
 
 /**
@@ -43,7 +53,7 @@ export function ticketStatus(tickets, ticketId) {
  * @returns {string} ticket status
  */
 export function simpleTicketStatus(tickets, ticketId) {
-  throw new Error('Please implement the simpleTicketStatus function.');
+  return tickets[ticketId] ?? 'invalid ticket !!!';
 }
 
 /**
@@ -53,5 +63,5 @@ export function simpleTicketStatus(tickets, ticketId) {
  * @returns {string | undefined} version
  */
 export function gtcVersion(visitor) {
-  throw new Error('Please implement the gtcVersion function.');
+  return visitor.gtc?.version;
 }
