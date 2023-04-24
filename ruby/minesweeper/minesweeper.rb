bccb43dcd3c3cfbed684ff48a5ca93976e86e4ee
Augusto Lopes <meleu.dev@gmail.com>
2023-04-24 09:09:06 -0300

    solve minesweeper.rb exercise

--- ruby/minesweeper/minesweeper.rb
+++ ruby/minesweeper/minesweeper.rb
@@ -1,7 +1,29 @@
-=begin
-Write your code for the 'Minesweeper' exercise in this file. Make the tests in
-`minesweeper_test.rb` pass.
+class Board
+  def self.transform(input)
+    raise ArgumentError unless input_valid?(input)
 
-To get started with TDD, see the `README.md` file in your
-`ruby/minesweeper` directory.
-=end
+    board = input.dup
+    for i in 1..(input.length - 2)
+      for j in 1..(input[i].length - 2)
+        next if input[i][j] == '*'
+
+        surroundings = input[i-1][j-1..j+1] + input[i+1][j-1..j+1] + input[i][j-1] + input[i][j+1]
+        counter = surroundings.count('*').to_s
+
+        board[i][j] = counter unless counter == '0'
+      end
+    end
+    board
+  end
+
+  def self.input_valid?(input)
+    return false unless input.all? { |line| line.length == input.first.length }
+
+    last_i = input.length - 1
+    return false unless input[0].squeeze == '+-+' && input[last_i].squeeze == '+-+'
+
+    input[1..last_i - 1].each do |line|
+      return false unless line.match?(/^\|[ *]+\|$/)
+    end
+  end
+end
