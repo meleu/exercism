18a1ebcaf7a6f6940468044a3877e81e3c54f79d
Augusto Lopes <meleu.dev@gmail.com>
2023-04-25 10:16:03 -0300

    provide methods as a module

--- ruby/list-ops/list_ops.rb
+++ ruby/list-ops/list_ops.rb
@@ -1,42 +1,44 @@
-class ListOps
-  def self.arrays(array)
-    sum = 0
-    array.each { |_n| sum += 1 }
-    sum
-  end
+module ListOps
+  class << self
+    def arrays(array)
+      sum = 0
+      array.each { |_n| sum += 1 }
+      sum
+    end
 
-  def self.reverser(array)
-    new = []
-    last_i = arrays(array) - 1
-    array.each_index { |i| new[last_i - i] = array[i] }
-    new
-  end
+    def reverser(array)
+      new = []
+      last_i = arrays(array) - 1
+      array.each_index { |i| new[last_i - i] = array[i] }
+      new
+    end
 
-  def self.concatter(array1, array2)
-    array1 + array2
-  end
+    def concatter(array1, array2)
+      array1 + array2
+    end
 
-  def self.mapper(array, &map_block)
-    new = []
-    array.each { |element| new << map_block.call(element) }
-    new
-  end
+    def mapper(array, &map_block)
+      new = []
+      array.each { |element| new << map_block.call(element) }
+      new
+    end
 
-  def self.filterer(array, &filter_block)
-    new = []
-    array.each { |element| new << element if filter_block.call(element) }
-    new
-  end
+    def filterer(array, &filter_block)
+      new = []
+      array.each { |element| new << element if filter_block.call(element) }
+      new
+    end
 
-  def self.sum_reducer(array)
-    sum = 0
-    array.each { |n| sum += n }
-    sum
-  end
+    def sum_reducer(array)
+      sum = 0
+      array.each { |n| sum += n }
+      sum
+    end
 
-  def self.factorial_reducer(array)
-    factorial = 1
-    array.each { |n| factorial *= n }
-    factorial
+    def factorial_reducer(array)
+      factorial = 1
+      array.each { |n| factorial *= n }
+      factorial
+    end
   end
 end
