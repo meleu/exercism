5334201fcd0902c1d7a4d95fd20476cdf81f4de1
meleu <8508804+meleu@users.noreply.github.com>
2024-01-27 17:14:26 -0300

    lasagna-master

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -1,12 +1,50 @@
 /// <reference path="./global.d.ts" />
 // @ts-check
 
-/**
- * Implement the functions needed to solve the exercise here.
- * Do not forget to export them so they are available for the
- * tests. Here an example of the syntax as reminder:
- *
- * export function yourFunction(...) {
- *   ...
- * }
- */
+export function cookingStatus(time) {
+  switch (time) {
+    case 0:
+      return "Lasagna is done.";
+    case undefined:
+      return "You forgot to set the timer.";
+    default:
+      return "Not done, please wait.";
+  }
+}
+
+export function preparationTime(layers, time = 2) {
+  return layers.length * time;
+}
+
+export function quantities(layers) {
+  const noodlesQuantity = 50;
+  const sauceQuantity = 0.2;
+  let noodles = 0;
+  let sauce = 0;
+
+  layers.forEach((layer) => {
+    noodles += layer === "noodles" ? noodlesQuantity : 0;
+    sauce += layer === "sauce" ? sauceQuantity : 0;
+  });
+
+  return {
+    noodles,
+    sauce,
+  };
+}
+
+export function addSecretIngredient(friendsList, myList) {
+  myList.push(friendsList[friendsList.length - 1]);
+}
+
+export function scaleRecipe(recipe, portions) {
+  const defaultPortions = 2;
+  const factor = portions / defaultPortions;
+  const newRecipe = {};
+
+  for (let ingredient in recipe) {
+    newRecipe[ingredient] = recipe[ingredient] * factor;
+  }
+
+  return newRecipe;
+}
