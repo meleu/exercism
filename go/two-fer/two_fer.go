4228ae621d324b56b96683cb71e98e0cc5d7b336
meleu <8508804+meleu@users.noreply.github.com>
2026-05-01 09:54:54 -0300

    Solve two-fer.go

---
+++ go/two-fer/two_fer.go
@@ -0,0 +1,13 @@
+// Package twofer should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package twofer
+
+import "fmt"
+
+// ShareWith should have a comment documenting it.
+func ShareWith(name string) string {
+	if name == "" {
+		name = "you"
+	}
+	return fmt.Sprintf("One for %s, one for me.", name)
+}
